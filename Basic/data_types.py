8862d69d71106f7a726b4ab4f4aaa9cd5be5a4aa
Nikhil Verma <115868225+nikhillvrma@users.noreply.github.com>
2026-01-28 19:17:10 +0530

    Delete Basic directory

--- Basic/data_types.py
+++
@@ -1,8 +0,0 @@
-x=55
-print(x, type(x))
-y=66.7
-print(y, type(y))
-name="Nikhl Verma"
-print(name, type(name))
-adult=True
-print(adult, type(adult))
--- Basic/escape.py
+++
@@ -1,4 +0,0 @@
-print("Hello Guys i am \"Nikhil\" Verma")
-print("i am not a \'gangster\', i am a \"Mafia\"")
-print("I am Nikhil Verma\n From Bihar")
-print("Name\t Passion\nNikhil\tMafia")
