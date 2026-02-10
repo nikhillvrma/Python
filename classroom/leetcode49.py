f7087b1bd1591d8abf58996d1dfbb470e4469f0c
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-02-10 10:04:10 +0530

    updated code of leetcode 49th problem

--- classroom/leetcode49.py
+++ classroom/leetcode49.py
@@ -2,15 +2,27 @@ class Solution(object):
     def groupAnagrams(self, strs):
         d = {}
 
-        for _ in strs:
-            key = tuple(sorted(_))
+        # method 1
+        # for _ in strs:
+        #     key = tuple(sorted(_))
 
-            if key in d:
-                d[key].append(_)
+        #     if key in d:
+        #         d[key].append(_)
+        #     else:
+        #         d[key] = [_]
+
+        # return list(d.values())
+
+        # method 2
+        for value in strs:
+            data = sorted(value)
+            data = "".join(data)
+            if data in d:
+                d[data].append(value)
             else:
-                d[key] = [_]
-
+                d[data] = [value]
         return list(d.values())
+
 
 strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
 obj = Solution()
