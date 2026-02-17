8cefb386b8dbbee9739ab0cff2eadb0588fb7822
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-02-17 10:50:00 +0530

    problems of leetcode on 17th Feb

---
+++ classroom/leetcode2.py
@@ -0,0 +1,23 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        new = ListNode()
+        result = new
+        total = carry = 0
+        while l1 or l2 or carry:
+            total = carry
+            if l1:
+                total += l1.val
+                l1 = l1.next
+            if l2:
+                total += l2.val
+                l2 = l2.next
+            num = total % 10
+            carry = total // 10
+            new.next = ListNode(num)
+            new = new.next
+        return result.next
