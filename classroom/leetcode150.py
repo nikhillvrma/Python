9c12883daf95f3185cbde38ba0b3d2bca9ad5b59
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-03-07 15:19:56 +0530

    New Problems of the Leetcode

--- classroom/leetcode150.py
+++ classroom/leetcode150.py
@@ -1,23 +1,23 @@
-class Solution:
-    def evalRPN(tokens):
-        ans = []
-        for i in tokens:
-            if i not in ["+", "-", "/", "*"]:
-                ans.append(int(i))
-            else:
-                a = ans.pop()
-                b = ans.pop()
-                if i == "+":
-                    ans.append(a + b)
-                elif i == "-":
-                    ans.append(b - a)
-                elif i == "*":
-                    ans.append(a * b)
-                elif i == "/":
-                    ans.append(int(b / a))
-        return ans[-1]
+# class Solution:
+#     def evalRPN(tokens):
+#         ans = []
+#         for i in tokens:
+#             if i not in ["+", "-", "/", "*"]:
+#                 ans.append(int(i))
+#             else:
+#                 a = ans.pop()
+#                 b = ans.pop()
+#                 if i == "+":
+#                     ans.append(a + b)
+#                 elif i == "-":
+#                     ans.append(b - a)
+#                 elif i == "*":
+#                     ans.append(a * b)
+#                 elif i == "/":
+#                     ans.append(int(b / a))
+#         return ans[-1]
 
-obj = Solution
-tokens = ["2", "1", "+", "3", "*"]
-r = obj.evalRPN(tokens)
-print(r)
+# obj = Solution
+# tokens = ["2", "1", "+", "3", "*"]
+# r = obj.evalRPN(tokens)
+# print(r)
---
+++ classroom/leetcode227.py
@@ -0,0 +1,9 @@
+class Solution:
+    def calculate(s):
+        return int(eval(s))
+
+
+obj = Solution()
+s = "3+4/7-2"
+r = obj.calculate(s)
+print(r)
