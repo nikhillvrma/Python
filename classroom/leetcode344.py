11cd59918d9364abc068e6797bdbfabb7415d8c2
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-03-28 19:00:11 +0530

    Leetcode problem number 344

---
+++ classroom/leetcode344.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        r = len(s)-1
+        l = 0
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
