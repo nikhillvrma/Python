fd693c3dc09fdc44fd26084159ef65c488a82154
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-04-02 10:49:27 +0530

    Inorder Traversal by Iterative Approach

--- classroom/leetcode94.py
+++ classroom/leetcode94.py
@@ -6,14 +6,30 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ans = []
+        # ans = []
 
-        def inorder(root):
-            if root == None:
-                return
-            inorder(root.left)
-            ans.append(root.val)
-            inorder(root.right)
+        # def inorder(root):
+        #     if root == None:
+        #         return
+        #     inorder(root.left)
+        #     ans.append(root.val)
+        #     inorder(root.right)
 
-        inorder(root)
-        return ans
+        # inorder(root)
+        # return ans
+
+        st = []
+        result = []
+        if root is None:
+            return []
+        curr = root
+        while curr != None or len(st) != 0:
+            if curr:
+                st.append(curr)
+                curr = curr.left
+            else:
+                curr = st[-1]
+                st.pop()
+                result.append(curr.val)
+                curr = curr.right
+        return result
