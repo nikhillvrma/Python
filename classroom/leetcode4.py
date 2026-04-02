bfe59c4ebe5e4f7ee61715dca8ee6155cdaf15a3
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-04-02 21:35:08 +0530

    Median of Two Sorted Arrays

--- classroom/leetcode4.py
+++ classroom/leetcode4.py
@@ -3,9 +3,9 @@ class Solution:
         num = []
         num.extend(nums1)
         num.extend(nums2)
+        num.sort()
         n = len(num)
-        if n%2 == 1:
-            median = num[n//2]
-        elif n%
-            median = (num[n//2] + num[n+1//2])/2
-        return median
+        if n % 2 == 1:
+            return num[n // 2]
+        elif n % 2 == 0:
+            return (num[n // 2 - 1] + num[n // 2]) / 2
