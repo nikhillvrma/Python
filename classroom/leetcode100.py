74c299bd99a5ee24c1754969e540baccc12c0886
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-04-09 10:37:43 +0530

    Same Tree

---
+++ classroom/leetcode100.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # if the inorder and the preorder of the both tree is same then both the trees are same
+        # so calculate the inorder and the preorder of the trees.
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
