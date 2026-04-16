09f15ccb1a44f738881e88a173cbaff606bdeaac
nikhil-jaihind1138 <nikhilverma1173@gmail.com>
2026-04-16 09:46:48 +0530

    Updated the leetcode problem 199

--- classroom/leetcode199.py
+++ classroom/leetcode199.py
@@ -6,8 +6,21 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-        right = self.rightSideView(root.right)
-        left = self.rightSideView(root.left)
-        return [root.val] + right + left[len(right) :]
+        # if not root:
+        #     return []
+        # right = self.rightSideView(root.right)
+        # left = self.rightSideView(root.left)
+        # return [root.val] + right + left[len(right):]
+        result = []
+
+        def dfs(node, level):
+
+            if not node:
+                return []
+            if level == len(result):
+                result.append(node.val)
+            dfs(node.right, level + 1)
+            dfs(node.left, level + 1)
+
+        dfs(root, 0)
+        return result
